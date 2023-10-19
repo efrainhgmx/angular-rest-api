013cbe034c1916b72340b8a73418d2138440ab71
Efrain Hernandez <efrainhg2050@gmail.com>
2023-10-19 16:09:30 -0600

    Styles for trasition in product detail

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ProductDetailComponent {
 
+  showDetail:boolean = false;
+
+
+  onShowDetail():void {
+    this.showDetail = !this.showDetail;
+  }
 }
