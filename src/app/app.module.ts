9bf03480069a8c741f30f6fdb61a0c004d79662b
Efrain Hernandez <efrainhg2050@gmail.com>
2023-10-18 18:32:04 -0600

    Adding material in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { CardProductComponent } from './components/card-product/card-product.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule  } from '@angular/material/card'
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
