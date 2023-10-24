b215ac5de9fe4f8e347470b0c56127a523b71b1a
Efrain Hernandez <efrainhg2050@gmail.com>
2023-10-24 17:17:24 -0600

    Update observable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/products.models';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   showDetailProduct: boolean = false;
+  products: Product[] = [];
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    console.log('On init')
+    this.productService.getAllProducts().subscribe(
+      {
+        next: (response: Product[]) => this.products = [...response],
+        error: console.error
+      }
+    );
+  }
 
   detailProduct(showDetail: Event | boolean):void {
     (showDetail) 
